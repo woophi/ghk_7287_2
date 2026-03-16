44351c5be8a0c2fdb57a1d56ab2ea87195b242f3
Konstantin Mikheev <mik.kon6610@gmail.com>
2026-03-16 12:11:17 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ export const App = () => {
               </PureCell.Content>
               <PureCell.Addon verticalAlign="top">
                 <Typography.Text view="primary-medium" weight="medium">
-                  {stock.price.toLocaleString('ru-RU')}&nbsp;₽
+                  {(stock.price * stock.nominal).toLocaleString('ru-RU')}&nbsp;{stock.currency || '₽'}
                 </Typography.Text>
               </PureCell.Addon>
             </PureCell>
@@ -129,7 +129,7 @@ export const App = () => {
               </PureCell.Content>
               <PureCell.Addon verticalAlign="top">
                 <Typography.Text view="primary-medium" weight="medium">
-                  {stock.price.toLocaleString('ru-RU')}&nbsp;₽
+                  {(stock.price * stock.nominal).toLocaleString('ru-RU')}&nbsp;{stock.currency || '₽'}
                 </Typography.Text>
               </PureCell.Addon>
             </PureCell>
